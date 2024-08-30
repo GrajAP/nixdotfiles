c1d9243f5033ff416be12b768b02ed40e156ec09
GrajAP <adampisarczyk2006@gmail.com>
2024-08-30 12:10:24 +0200

    340 current  2024-08-30 12:10:22  24.05.20240425.7bb2ccd  6.8.7                           *

--- rebuild.sh
+++ rebuild.sh
@@ -31,8 +31,5 @@ current=$(nixos-rebuild list-generations | grep current)
 # Commit all changes witih the generation metadata
 git commit -am "$current"
 
-# Back to where you were
-popd
-
 # Notify all OK!
 notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
