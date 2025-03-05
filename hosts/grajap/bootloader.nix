d3a8a57ab49a69b0f8db919d7125f280d32cf049
GrajAP <adampisarczyk2006@gmail.com>
2025-03-05 08:14:33 +0100

    439 current  2025-03-05 08:14:19  25.05.20250303.ba487db  6.13.5-zen1                          *

--- hosts/grajap/bootloader.nix
+++
@@ -1,5 +0,0 @@
-{...}: {
-  boot = {
-    initrd.luks.devices."luks-b02f9d12-7cd0-44dd-a637-c9b7a4f84049".device = "/dev/disk/by-uuid/b02f9d12-7cd0-44dd-a637-c9b7a4f84049";
-  };
-}
--- hosts/grajap/default.nix
+++ hosts/grajap/default.nix
@@ -5,8 +5,6 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    ./system.nix
-    ./bootloader.nix
   ];
   environment.systemPackages = with pkgs; [
     acpi
@@ -15,6 +13,56 @@
 
   networking.hostName = "grajap"; # Define your hostname.
   services = {
+    ollama = {
+      enable = true;
+      #acceleration = "rocm";
+      environmentVariables = {
+        HCC_AMDGPU_TARGET = "gfx803"; # used to be necessary, but doesn't seem to anymore
+      };
+      rocmOverrideGfx = "8.0.3";
+    };
+
+    kanata = {
+      enable = true;
+      keyboards = {
+        internalKeyboard = {
+          devices = [
+            "/dev/input/by-path/pci-0000:0e:00.3-usb-0:1:1.0-event-kbd"
+            #"/dev/input/by-path/pci-0000:0e:00.3-usb-0:2:1.1-event-kbd"
+            #"/dev/input/by-path/pci-0000:0e:00.3-usb-0:1:1.2-event-kbd"
+          ];
+          extraDefCfg = "process-unmapped-keys yes";
+          config = ''
+
+            (defsrc
+              caps a s d f j k l ;
+            )
+            (defvar
+              tap-time 200
+              hold-time 200
+            )
+
+            (defalias
+              escctrl (tap-hold $tap-time $hold-time esc lctl)
+              a (multi f24 (tap-hold $tap-time $hold-time a lmet))
+              s (multi f24 (tap-hold $tap-time $hold-time s ralt))
+              d (multi f24 (tap-hold $tap-time $hold-time d lsft))
+              f (multi f24 (tap-hold $tap-time $hold-time f lctl))
+              j (multi f24 (tap-hold $tap-time $hold-time j lctl))
+              k (multi f24 (tap-hold $tap-time $hold-time k lsft))
+              l (multi f24 (tap-hold $tap-time $hold-time l ralt))
+              ; (multi f24 (tap-hold $tap-time $hold-time ; lmet))
+            )
+
+            (deflayer base
+              @escctrl @a @s @d @f @j @k @l @;
+            )
+
+
+          '';
+        };
+      };
+    };
     fprintd.enable = true;
     thermald.enable = true;
     xserver.videoDrivers = ["amdgpu"];
@@ -24,6 +72,7 @@
     };
   };
   boot = {
+    initrd.luks.devices."luks-b02f9d12-7cd0-44dd-a637-c9b7a4f84049".device = "/dev/disk/by-uuid/b02f9d12-7cd0-44dd-a637-c9b7a4f84049";
     kernelModules = ["acpi_call"];
     extraModulePackages = with config.boot.kernelPackages;
       [
