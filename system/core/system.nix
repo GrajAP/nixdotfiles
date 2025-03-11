cf4a0b6151550fe62badc89eafc3eb0249c6c158
GrajAP <adampisarczyk2006@gmail.com>
2025-03-11 12:50:05 +0100

    454 current  2025-03-11 12:49:47  25.05.20250309.e3e32b6  6.13.5-zen1                          *

--- system/core/system.nix
+++ system/core/system.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   services = {
-    wordpress.sites."localhost" = {};
+    #wordpress.sites."localhost" = {};
     dbus = {
       packages = with pkgs; [dconf gcr udisks2];
       enable = true;
