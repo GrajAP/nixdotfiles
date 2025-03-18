084096e3690c45dd965b11a034baa4c2af5e4e8a
GrajAP <adampisarczyk2006@gmail.com>
2025-03-18 09:09:26 +0100

    473 current  2025-03-18 08:49:57  25.05.20250315.c80f6a7  6.13.7-zen1                          *

--- home/rice/waybar/wttr.py
+++ home/rice/waybar/wttr.py
@@ -58,7 +58,7 @@ WEATHER_CODES = {
 data = {}
 
 
-weather = requests.get("https://wttr.in/?format=j1").json()
+weather = requests.get("https://wttr.in/Dąbrowa-Górnicza?format=j1").json()
 
 
 def format_time(time):
