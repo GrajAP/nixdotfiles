b4ff24dd29e723180dc8cd77e293562e7f36b904
GrajAP <adampisarczyk2006@gmail.com>
2025-06-03 20:23:21 +0200

    342 current  2025-06-03 20:23:12  25.11.20250531.910796c  6.14.7-zen1                          *

--- home/rice/waybar/default.nix
+++ home/rice/waybar/default.nix
@@ -34,20 +34,20 @@ in {
         modules-right = ["pulseaudio" "bluetooth" "network" "clock"];
         "hyprland/workspaces" = {
           on-click = "activate";
-          format = "{icon}";
+          format = "{icon} {title}";
           active-only = false;
-          format-icons = {
-            "1" = "一";
-            "2" = "二";
-            "3" = "三";
-            "4" = "四";
-            "5" = "五";
-            "6" = "六";
-            "7" = "七";
-            "8" = "八";
-            "9" = "九";
-            "10" = "十";
-          };
+          #        format-icons = {
+          #          "1" = "一";
+          #          "2" = "二";
+          #          "3" = "三";
+          #          "4" = "四";
+          #          "5" = "五";
+          #          "6" = "六";
+          #          "7" = "七";
+          #          "8" = "八";
+          #          "9" = "九";
+          #          "10" = "十";
+          #        };
 
           persistent_workspaces = {
             "*" = 0;
